873220f113f40620f94401599fb36c2fd5d0ebd3
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-10-16 23:04:56 -0300

    main(tweet.py): Finished API communication.

--- src/tweet.py
+++ src/tweet.py
@@ -7,4 +7,13 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 user = api.me()
 
-print(user.name)
+keyword = 'Python'
+numbers_of_tweets = 5
+
+for tweet in tweepy.Cursor(api.search, keyword).items(numbers_of_tweets):
+    try:
+        print(tweet.text)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
