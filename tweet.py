0d3ae8d46bb501a91bc10680cc757d1b97604ae4
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-10-16 22:43:05 -0300

    main(tweet.py): Added base code.

--- tweet.py
+++ tweet.py
@@ -1,4 +1,10 @@
 import tweepy
+from env import *
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token()
+auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+user = api.me()
+
+print(user.name)
